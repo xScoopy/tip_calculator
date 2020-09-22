8dfd40cb974d6296b13142c8364795205d4965af
xScoopy <JLeary070@gmail.com>
2020-09-22 16:20:41 -0700

    added event listeners for input on both bill and tip inputs

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ const display = document.querySelector('#display')
 const billInput = document.querySelector('#bill')
 const tipInput = document.querySelector('#tip')
 
+//Event Listeners
+billInput.addEventListener('input', calculateTip)
+tipInput.addEventListener('input', calculateTip)
+
 console.log(display.innerHTML) //printes the text inside the tags
 console.log(billInput.value) //prints the value entered into the input
 
