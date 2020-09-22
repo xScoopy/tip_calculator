47ea6220a9cc817e72968187b6c66b0c0381938a
xScoopy <JLeary070@gmail.com>
2020-09-22 16:58:52 -0700

    added a number of people input and added functionality to the function

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 const display = document.querySelector('#display')
 const billInput = document.querySelector('#bill')
 const tipInput = document.querySelector('#tip')
+const peopleInput = document.querySelector('#people')
 
 //Event Listeners
 billInput.addEventListener('input', calculateTip)
 tipInput.addEventListener('input', calculateTip)
+peopleInput.addEventListener('input', calculateTip)
 
 console.log(display.innerHTML) //printes the text inside the tags
 console.log(billInput.value) //prints the value entered into the input
@@ -15,8 +17,8 @@ console.log(billInput.value) //prints the value entered into the input
 function calculateTip() {
     const billValue = billInput.value
     const tipValue = tipInput.value 
-
-    const tipAmount = billValue * tipValue / 100
+    const peopleValue = peopleInput.value
+    const tipAmount = billValue * tipValue / 100 / peopleValue
 
     display.innerHTML = tipAmount.toFixed(2)
 }
