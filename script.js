825c49d2aae0b6fd7fee553db35efb3f000e1a95
xScoopy <JLeary070@gmail.com>
2020-09-22 17:33:04 -0700

    added not workign functionality for mutliple people.

--- script.js
+++ script.js
@@ -1,25 +1,28 @@
 const display = document.querySelector('#display')
+const totalDisplay = document.querySelector('#total')
 const billInput = document.querySelector('#bill')
 const tipInput = document.querySelector('#tip')
 const peopleInput = document.querySelector('#people')
+
 
 //Event Listeners
 billInput.addEventListener('input', calculateTip)
 tipInput.addEventListener('input', calculateTip)
 peopleInput.addEventListener('input', calculateTip)
 
-console.log(display.innerHTML) //printes the text inside the tags
-console.log(billInput.value) //prints the value entered into the input
+//console.log(display.innerHTML) //printes the text inside the tags
+//console.log(billInput.value) //prints the value entered into the input
 
 //billInput.value = 999.88 // Set the value to 999.88
 
 // Declare calculate tip function
 function calculateTip() {
-    const billValue = billInput.value
-    const tipValue = tipInput.value 
-    const peopleValue = peopleInput.value
-    const tipAmount = billValue * tipValue / 100 / peopleValue
-
+    const billValue = parseFloat(billInput.value)
+    const tipValue = parseInt(tipInput.value) 
+    const peopleValue = parseInt(peopleInput.value)
+    const tipAmount = parseFloat(billValue * tipValue / 100 / peopleValue)
+    const totalAmount = parseFloat(billValue + tipAmount)
     display.innerHTML = tipAmount.toFixed(2)
+    totalDisplay.innerHTML = totalAmount.toFixed(2)
 }
 calculateTip()
